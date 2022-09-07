63e1fc7795e88f852a29b92f60f0382f0183cc4f
cuneytsoylu <110092795+cuneytsoylu@users.noreply.github.com>
2022-09-07 19:36:27 +0300

    ss

---
+++ ArrayCalısma/A01.java
@@ -0,0 +1,12 @@
+package ArrayCalısma;
+
+import java.util.Arrays;
+
+public class A01 {
+    public static void main(String[] args) {
+        int [] a=new int[10];
+        a[5]=32;
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a));
+    }
+}
